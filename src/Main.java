cf9e9ab01af8d6173a1678b7ef418f84adebb03a
Irek <ireneusz.piskorski@le-campus-numerique.fr>
2024-01-15 10:33:27 +0100

    Ex 2A.2 - OK

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("Ex 2!");
 
-        int i = 1;
-        while (i<=100){
-            System.out.print(i + ", ");
-            if (i % 10 == 0){
+        System.out.println("for:");
+        for (int j = 1; j <=100 ; j++) {
+            System.out.print(j + ", ");
+            if (j % 10 == 0){
                 System.out.println(" ");
             }
-            i++;
         }
     }
 }
